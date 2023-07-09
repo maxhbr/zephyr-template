1240a2c79d101e8b9760e97676904763dd01dbfd
Maximilian Huber <gh@maxhbr.de>
2023-07-09 17:44:46 +0200

    update init
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd "$ROOT"
-west init -l ./app
-west update
+[[ -d ".west" ]] || west init -l ./app
+west update --narrow #-f always
+west blobs fetch hal_espressif
+west config -l
 west zephyr-export
