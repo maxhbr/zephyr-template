65ebd11507181452ce6a037f295712becc928643
Maximilian Huber <gh@maxhbr.de>
2023-08-09 15:29:56 +0200

    minor changes
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- docker/software/install.uniflash_sl.sh
+++ docker/software/install.uniflash_sl.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
+# apt-get install -y libc6-i386 libusb-0.1-4 libgconf-2-4 libncurses5 libpython2.7 libtinfo5
+
 UNIFLASH_SL_URL="https://dr-download.ti.com/software-development/software-programming-tool/MD-QeJBJLj8gq/8.3.0/uniflash_sl.8.3.0.4307.run"
 wget \
      -nv "$UNIFLASH_SL_URL" \
